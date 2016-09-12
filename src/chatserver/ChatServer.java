11ef63e0f5ea7543b75f839ae8b4fed37490cd5a
antonwass <anton_wass@hotmail.com>
2016-09-12 14:56:42 +0200

    Updated with methods for broadcast, who, disconnect.
    Added receive loop in clienthandler.
    
    TODO: gå igenom allting och se så att det faktiskt är rätt.

--- src/chatserver/ChatServer.java
+++ src/chatserver/ChatServer.java
@@ -3,6 +3,7 @@ package chatserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by Christian on 2016-09-08.
@@ -10,17 +11,21 @@ import java.net.Socket;
 public class ChatServer {
     private static final int PORT = 12345;
     ServerSocket serverSocket = null;
+    ArrayList<ClientHandler> clients = null;
+    private boolean listening = false;
 
     public void start() throws IOException{
         try{
+            clients = new ArrayList<>();
             serverSocket = new ServerSocket(PORT);
             //Start listending after clients and thread it up.
-            while(true){
+            while(listening){
                 System.out.println("Waiting for client to connect...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted client from "+clientSocket.getInetAddress());
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                ClientHandler clientHandler = new ClientHandler(clientSocket, this);
                 clientHandler.start();
+                clients.add(clientHandler);
             }
         }finally{
             try{
@@ -28,6 +33,40 @@ public class ChatServer {
             }catch (Exception e){
                 System.out.println(e);
             }
+        }
+    }
+
+    /**
+     * Sends list of all connected clients to the client who requested it.
+     * @param target The client who requested the list
+     */
+    public synchronized void whoClients(ClientHandler target){
+        String clientList = "";
+
+        for(ClientHandler ch : clients){
+            clientList += ch.getNickname() + " \n";
+        }
+
+        target.send(clientList);
+    }
+
+    /**
+     * Attempts to disconnect a client and remove it from the clienthandler list.
+     * @param ch The client to disconnect
+     */
+    public synchronized void disconnectClient(ClientHandler ch){
+        ch.disconnect();
+        clients.remove(ch);
+    }
+
+    /**
+     * Send a message to all connected clients
+     * TODO: even the client who sent the message??
+     * @param msg
+     */
+    public synchronized void broadcast(String msg){
+        for(ClientHandler ch : clients){
+            ch.send(msg);
         }
     }
 
--- src/chatserver/ClientHandler.java
+++ src/chatserver/ClientHandler.java
@@ -1,8 +1,11 @@
 package chatserver;
 
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -13,20 +16,30 @@ public class ClientHandler extends Thread {
 
     private Socket socket = null;
     private PrintWriter out = null;
+    private ChatServer server = null;
+    private boolean connected = false;
+    private BufferedReader in = null;
+    private String nickName = "Unnamed";
 
-    public ClientHandler(Socket socket){
+    public ClientHandler(Socket socket, ChatServer server){
         this.socket = socket;
+        this.server = server;
+        this.connected = true;
     }
 
     @Override
     public void run() {
         try{
 
-            out = new PrintWriter(socket.getOutputStream(),true);
-            out.println("Welcome "+socket.getInetAddress() + ":"+ socket.getPort());
+            this.out = new PrintWriter(socket.getOutputStream(),true);
+            this.in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+            this.out.println("Welcome "+socket.getInetAddress() + ":"+ socket.getPort());
 
         } catch (IOException e) {
             System.out.println(e);
+
         } finally {
             try{
                 if(socket != null) socket.close();
@@ -40,6 +53,86 @@ public class ClientHandler extends Thread {
                 }
             }
         }
+    }
+
+    public void setNickname(String nick){
+        this.nickName = nick;
+    }
+
+    public String getNickname(){
+        return this.nickName;
+    }
+
+    /**
+     * Receive until the client is no longer connected.
+     */
+    private void receive(){
+        try{
+            while(connected){
+                String received = in.readLine();
+                processMessage(received);
+            }
+        }catch(Exception e){
+
+        }finally {
+            server.disconnectClient(this);
+        }
+    }
+
+    private void processMessage(String message){
+        String[] split = message.split(" ");
+        switch(split[0]){
+            //commands
+            case "/quit":
+                server.disconnectClient(this);
+                break;
+            case "/who":
+                //send all user names currently connected
+                break;
+            default:
+                server.broadcast(message);
+                //broadcast message
+                break;
+
+        }
+    }
+
+    /**
+     * Send a string to client.
+     * @param msg The message to be sent.
+     */
+    public synchronized void send(String msg){
+        out.println(msg);
+    }
+
+    /**
+     * Do not call this method directly from this class.
+     * That will cause the server to keep this client in its list.
+     * Write logic to handle this situation?
+     */
+    public synchronized void disconnect(){
+        if(connected == false){ //To protect from circular calls.
+            return;
+        }
+
+        try{
+            if(in != null)
+                in.close();
+        }catch(IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+
+        if(out != null)
+            out.close();
+
+        if(socket != null){
+            try{
+                socket.close();
+            }catch(IOException ioe){
+                System.out.println(ioe.getMessage());
+            }
+        }
+        connected = false;
     }
 
 
