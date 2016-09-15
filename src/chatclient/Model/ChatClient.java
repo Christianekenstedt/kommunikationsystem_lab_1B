485e284ce86dd9735f77dfcac6117e48f75e98aa
christianekenstedt <christian>
2016-09-15 10:23:28 +0200

    threading client

--- src/chatclient/Model/ChatClient.java
+++ src/chatclient/Model/ChatClient.java
@@ -5,7 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -19,8 +21,11 @@ public class ChatClient {
 
     private Socket socket = null;
     private BufferedReader in = null;
+    private PrintWriter out = null;
 
     private TextArea outputArea;
+
+    private boolean connected = false;
 
     public ChatClient(String serverName,TextArea outputArea){
         this.serverName = serverName;
@@ -31,12 +36,20 @@ public class ChatClient {
         if (serverName == null) System.exit(0);
 
         try{
-            outputArea.setText("Connecting to "+serverName+"...");
+            outputArea.setText("Connecting to "+serverName+"...\n");
             InetAddress serverAddress = InetAddress.getByName(serverName);
             socket = new Socket(serverAddress, PORT_SERVER);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String input = in.readLine();
-            outputArea.appendText("\n"+input);
+            connected = true;
+            this.out = new PrintWriter(socket.getOutputStream(),true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            new Thread(){
+                @Override
+                public void run(){
+                    recive();
+                }
+            }.run();
+
+
         }finally {
             try {
                 if (socket != null) {
@@ -47,17 +60,48 @@ public class ChatClient {
             } catch (Exception e) {
                 System.out.println(e);
                 outputArea.appendText("\n"+"Connection lost.");
-            } finally {
-                try {
-                    if (in != null) in.close();
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
             }
+            try {
+                if (in != null) in.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            try {
+                if (out != null) out.close();
+            }catch (Exception e){
+                System.out.println(e);
+            }
+
         }
     }
 
-    public void send(String message) {
+    public void send(String message) throws IOException{
+        if (message.equals("/exit")){
+            connected = false;
+        }
+        try{
+            this.out.println(message);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    private void recive(){
+        try{
+            while (connected){
+                String input = in.readLine();
+                printMessage(input);
+            }
+        }catch(IOException e){
+            System.out.println(e);
+            // Stänga?
+        }
+    }
+
+    private void printMessage(String message){
+        synchronized (outputArea){
+            outputArea.appendText(message);
+        }
 
     }
 }
